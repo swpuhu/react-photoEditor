946cce8b6c6d2f0b19e84ffbf525828ae979550b
huyunhe <huyunhe@sparkedu.com>
2024-07-02 14:09:12 +0800

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Layout } from 'antd';
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <Layout style={{ minHeight: '100vh' }}></Layout>
+        </>
+    );
+}
+
+export default App;
